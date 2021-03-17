da72eb78f34686cbd0be79cc84cbd470e84cf95d
jackieeee <jglimp@users.noreply.github.com>
2021-03-17 17:37:04 -0600

    setup

---
+++ components/Logo.vue
@@ -0,0 +1,16 @@
+<template>
+  <img src="/cookies-cover.png" alt="cookies" title="Logo" />
+</template>
+
+<style>
+.NuxtLogo {
+  animation: 1s appear;
+  margin: auto;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+}
+</style>
